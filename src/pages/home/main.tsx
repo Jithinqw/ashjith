9efb390c7a56c8ba472cb3946c997f29ffaf1a5e
Jithinqw <zaccydiai7@gmail.com>
2025-02-25 10:10:41 -0700

    Add project

--- src/pages/home/main.tsx
+++ src/pages/home/main.tsx
@@ -12,7 +12,18 @@ const Main = () => {
   return (
     <div>
       {/* Head */}
-      
+      <header className="relative">
+        <div className="px-4 sm:px-6 md:px-8 main">
+          <div className="absolute inset-0 bottom-10 bg-bottom bg-no-repeat">
+            <div className="absolute inset-0 bg-grid-slate-900/[0.04] bg-[bottom_1px_center] dark:bg-grid-slate-400/[0.05] dark:bg-bottom dark:border-b dark:border-slate-100/5"></div>
+          </div>
+          <Header />
+        </div>
+      </header>
+      {/* Body */}
+      <div className="overflow-hidden">
+        <InviteHero />
+      </div>
       {/* Footer */}
       <Footer />
     </div>
